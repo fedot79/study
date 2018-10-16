b2b6d5b7c07ce237bfdf49066e55e3a5e313f750
fedot79 <39196719+fedot79@users.noreply.github.com>
2018-10-16 14:03:31 +0300

    доделал админку

--- admin/blocks/content.php
+++ admin/blocks/content.php
@@ -1,5 +1,7 @@
 <main>
 <h4><a href="../categories.php">Категории</a>||<a href="../add_cat.php"> Добавить категорию</a>||<a href="../delete_cat.php"> Удалить категорию</a>||<a href="../update_cat.php">Обновить категорию</a></h4>
  <h4><a href="../add_goods.php">Добавить товары</a>||<h4><a href="../goods.php">Товары</a>||<a href="../update_goods.php">Обновить товары</a>||<a href="../delete_goods.php">Удалить товары</a></h4>
+     <h4><a href="show_orders.php">Проверить заказы.</a></h4>
 <p><a href="blocks/auth.php">Выйти из админ. зоны</a></p>
+
 </main>
